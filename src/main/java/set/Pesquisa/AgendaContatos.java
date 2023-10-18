3fca03295c600d47faf37704b91bd4025616200e
Thaina Colli <thaina.alves@first-tech.net>
2023-10-18 19:18:18 -0300

    Realizando pesquisa com Set

---
+++ src/main/java/set/Pesquisa/AgendaContatos.java
@@ -0,0 +1,63 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato() {
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato c : contatoSet) {
+            if(c.getNome().startsWith(nome)) {
+              contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato (String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet) {
+            if(c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 0);
+        agendaContatos.adicionarContato("Camila Cavalcante", 11111111);
+        agendaContatos.adicionarContato("Camila DIO", 654987);
+        agendaContatos.adicionarContato("Clara", 654987);
+
+        agendaContatos.exibirContato();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Clara"));
+
+        System.out.println(agendaContatos.atualizarNumeroContato("Clara", 12345));
+
+        ;
+    }
+
+
+}
---
+++ src/main/java/set/Pesquisa/ListaTarefas.java
@@ -0,0 +1,121 @@
+package set.Pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ListaTarefas {
+
+    private Set<Tarefa> tarefaSet;
+
+    public ListaTarefas() {
+        this.tarefaSet = new HashSet<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaSet.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa (String descricao) {
+        Tarefa tarefaParaRemover = null;
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefaParaRemover = t;
+                break;
+            }
+        }
+        tarefaSet.remove(tarefaParaRemover);
+    }
+
+    public void exibirTarefas() {
+        System.out.println(tarefaSet);
+    }
+
+    public int contarTarefas() {
+       return tarefaSet.size();
+    }
+
+    public Set<Tarefa> obterTarefasConcluidas() {
+        Set<Tarefa> tarefasConcluidas = new HashSet<>();
+        for (Tarefa t : tarefaSet) {
+            if (t.isTarefaConcluida()) {
+                tarefasConcluidas.add(t);
+            }
+        }
+
+        return tarefasConcluidas;
+    }
+
+    public Set<Tarefa> tarefasPendentes() {
+        Set<Tarefa> tarefasPendentes = new HashSet<>();
+        for (Tarefa t : tarefaSet) {
+            if (!t.isTarefaConcluida()) {
+                tarefasPendentes.add(t);
+            }
+        }
+
+        return  tarefasPendentes;
+    }
+
+    public void marcarTarefaConcluida(String descricao) {
+        Tarefa tarefaParaMarcar = null;
+        for (Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                t.setTarefaConcluida(true);
+            }
+        }
+    }
+
+    public void marcarTarefaPendente (String descricao) {
+        Tarefa tarefaPendente = null;
+        for(Tarefa t : tarefaSet) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefaPendente = t;
+                break;
+            }
+        }
+        if (tarefaPendente != null) {
+            if (tarefaPendente.isTarefaConcluida()) {
+                tarefaPendente.setTarefaConcluida(false);
+            }
+        }
+    }
+
+    public void limparListaTarefa() {
+        tarefaSet.clear();
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        listaTarefas.adicionarTarefa("Estudar Java");
+        listaTarefas.adicionarTarefa("Fazer exercícios físicos");
+        listaTarefas.adicionarTarefa("Organizar a mesa de trabalho");
+        listaTarefas.adicionarTarefa("Ler livro");
+        listaTarefas.adicionarTarefa("Preparar apresentação");
+
+        listaTarefas.exibirTarefas();
+
+        listaTarefas.removerTarefa("Fazer exercícios físicos");
+        listaTarefas.exibirTarefas();
+
+        System.out.println("Total de tarefas na lista: " + listaTarefas.contarTarefas());
+
+        System.out.println(listaTarefas.tarefasPendentes());
+
+        listaTarefas.marcarTarefaConcluida("Ler livro");
+        listaTarefas.marcarTarefaConcluida("Estudar Java");
+
+        System.out.println(listaTarefas.obterTarefasConcluidas());
+
+        listaTarefas.marcarTarefaPendente("Estudar Java");
+        listaTarefas.exibirTarefas();
+
+        listaTarefas.limparListaTarefa();
+        listaTarefas.exibirTarefas();
+
+
+
+
+    }
+
+}
